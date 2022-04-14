161a46c7f2fad3a442e1a85be40d9259bbcab221
brachintosh <bedendo.br@gmail.com>
2022-04-14 19:43:58 -0300

    card-content-typeWriteEffect

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import TypeWriterEffect from 'react-typewriter-effect';
 import {
    makeStyles, Typography, Card, CardMedia, CardContent, Box
   } from "@material-ui/core";
@@ -18,7 +19,30 @@ const classes = useStyles();
               <img src={author} title="picture-brachintosh" className={classes.imgAuthor} />
             </Box>
             <CardContent className={classes.cardContent} >
-              Soy el contenido de la tarjeta
+              <TypeWriterEffect 
+                text="Hi,I'm Brian Ezequiel Bedendo."
+                textStyle={{
+                  fontSize:'1.5rem',
+                  fontWeight: 600,
+                  color: '#795548',
+                }}
+                cursorColor='#EEEEEE'
+                startDelay={200}
+                typeSpeed={100}
+              />
+              
+              <TypeWriterEffect 
+                text=" 🚀 Enthusiastic Dev.   "
+                textStyle={{
+                  fontSize:'1rem',
+                  fontWeight: 450,
+                  color: '#795548',
+                }}
+                cursorColor='#EEEEEE'
+                startDelay={3800}
+                typeSpeed={100}
+              />
+              
             </CardContent>
           </Card>
 
@@ -44,15 +68,18 @@ const useStyles = makeStyles((theme) =>({
       marginTop: 10,
     },
     cardContainer: {
+      position: 'relative',
       marginTop: theme.spacing(2.5),
-      color: '#795548',
+      // fontWeight: 450,
+      // color: '#795548',
+      backgroundColor: '#EEEEEE',
       border: 'solid #D7A86E 1.8px',
       width: '85vw',
       height: '70vh',
       display: 'flex',
-      backgroundColor: '#EEEEEE'
     },
     boxImg: {
+      position: 'relative',
       width: '50vw',
       height: '50vh',
       display: 'flex',
@@ -60,10 +87,13 @@ const useStyles = makeStyles((theme) =>({
       alignSelf: 'center',
     },
     imgAuthor: {
-      marginLeft: 4,
+      margin: theme.spacing(0.8),
       height: 'auto',
       border: 'solid #D7A86E 1.2px',
       borderRadius: '50%',
+    },
+    cardContent: {
+
     },
 
   }))
