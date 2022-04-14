393684f0fe1606ef1ef8f2baa0ff197eaeffdc3a
brachintosh <bedendo.br@gmail.com>
2022-04-14 19:06:10 -0300

    profile-img

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {
-   makeStyles, Typography, Card, CardMedia 
+   makeStyles, Typography, Card, CardMedia, CardContent, Box
   } from "@material-ui/core";
+import author from '../images/brachintosh-Img.jpg';
 
 const About = ({ title, dark, id }) => {
 
@@ -13,7 +14,12 @@ const classes = useStyles();
           <Typography variant="h3" >{title}:</Typography>
 
           <Card className={classes.cardContainer} >
-            <CardMedia className={classes.cardMedia} />
+            <Box className={classes.boxImg} >
+              <img src={author} title="picture-brachintosh" className={classes.imgAuthor} />
+            </Box>
+            <CardContent className={classes.cardContent} >
+              Soy el contenido de la tarjeta
+            </CardContent>
           </Card>
 
         </div>
@@ -35,8 +41,27 @@ const useStyles = makeStyles((theme) =>({
     sectionContent: {
       maxWidth: "85vw",
       margin: "0 auto",
-      paddingTop: 9,
     },
+    cardContainer: {
+      border: 'solid #D7A86E 1.8px',
+      width: '85vw',
+      height: '70vh',
+      display: 'flex',
+    },
+    boxImg: {
+      width: '50vw',
+      height: '50vh',
+      display: 'flex',
+      justifyContent: 'start',
+      alignSelf: 'center',
+    },
+    imgAuthor: {
+      marginLeft: 4,
+      height: 'auto',
+      border: 'solid #D7A86E 1.2px',
+      borderRadius: '50%',
+    },
+
   }))
 
 export default About
