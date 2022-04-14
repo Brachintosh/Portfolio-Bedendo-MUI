d06374051af20706f09fb63ba8bef72588a27c40
brachintosh <bedendo.br@gmail.com>
2022-04-14 16:21:09 -0300

    sideBar-responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,37 @@
-import { AppBar, makeStyles, Toolbar, Typography, List } from '@material-ui/core';
-import React from 'react';
+import { 
+  AppBar, makeStyles, Toolbar, Typography, List, IconButton, Drawer, Divider, ListItem, ListItemIcon, 
+      } from '@material-ui/core';
+import React, { useState } from 'react';
 import logo from "../images/coding1a.png";
 import { Link, animateScroll as scroll } from "react-scroll";
 import BuildTwoToneIcon from '@material-ui/icons/BuildTwoTone';
 import ContactMailTwoToneIcon from '@material-ui/icons/ContactMailTwoTone';
-import EmailTwoToneIcon from '@material-ui/icons/EmailTwoTone';
+// import EmailTwoToneIcon from '@material-ui/icons/EmailTwoTone';
 import EmojiObjectsTwoToneIcon from '@material-ui/icons/EmojiObjectsTwoTone';
 import InfoTwoToneIcon from '@material-ui/icons/InfoTwoTone';
-//  InfoOutlinedIcon, InfoTwoTone, 
-
+import MenuIcon from  '@material-ui/icons/Menu';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 const linksNav = [
     {
         id: 'about',
         text: 'About',
-        icon: <InfoTwoToneIcon />,
+        icon: <InfoTwoToneIcon fontSize='large' />,
     },
     {
         id: 'skills',
         text: 'Skills',
-        icon: <EmojiObjectsTwoToneIcon />,
+        icon: <EmojiObjectsTwoToneIcon fontSize='large' />,
     },
     {
         id: 'work',
         text: 'My Works',
-        icon: <BuildTwoToneIcon />,
+        icon: <BuildTwoToneIcon fontSize='large' />,
     },
     {
         id: 'contact',
         text: 'Get in touch',
-        icon: <ContactMailTwoToneIcon />,
+        icon: <ContactMailTwoToneIcon fontSize='large' />,
     },
 ]
 
@@ -37,7 +39,10 @@ const NavBar = () => {
 
 const classes = useStyles();
 
+const [open, setOpen] = useState(false);
+
   return (
+  <>
     <AppBar position="sticky" className={classes.root} >
         <Toolbar className={classes.toolbar} >
           <Typography variant="h6" component="div" className={classes.nameStyle}>
@@ -56,9 +61,42 @@ const classes = useStyles();
               ))
             }
           </List>
+          {/* HAMB.MENU */}
+          <IconButton  edge='end' className={classes.menuButton} onClick={() => setOpen(!open) } >
+            <MenuIcon  fonstSize='large' />
+          </IconButton>
           
         </Toolbar>
     </AppBar>
+
+  <div className={classes.drawer} >
+    <Drawer anchor="right" open={open} onClose={() => setOpen(false) } >
+      {/* EXIT.ICON */}
+      <IconButton className={classes.menuDrawer} onClick={() => setOpen(false)}  >
+        <CancelIcon />
+      </IconButton>
+      <Divider />
+
+      {
+        linksNav?.map(({ id, text, icon }, index)=> (
+          <Link className={classes.linksDrawer}
+            key={index} to={id} spy={true} activeClass="active" smooth={true} duration={750} offset={-60}
+            onClick={() => setOpen(!open)} >
+            
+            <ListItem component='h4' >
+              <span>
+                <ListItemIcon >
+                  {icon}
+                </ListItemIcon>
+              </span>{text}
+            </ListItem>
+          </Link>
+        ))
+      }
+      
+    </Drawer>
+  </div>
+  </>
   )
 };
 
@@ -100,10 +138,48 @@ const useStyles = makeStyles((theme) =>({
               cursor: 'pointer',
               fontWeight: 'bold',
               transition: 'all ease 0.6s',
-              color: "#FF5353",
+              color: "tomato",
               textShadow: ".8px .65px  #A0522D",
+              borderBottom: "2.5px solid tomato",
+              marginTop: "2.5px",
           },
       },
+    },
+    menuButton: {
+      display:'none',
+      [theme.breakpoints.down('sm')]: {
+        display: 'block',
+        position: 'absolute',
+        color: '#A0522D',
+        top: 8,
+        right: 20,
+      }
+    },
+    drawer: {
+      backgroundColor: '#FCFF53',
+    },
+    linksDrawer: {
+      [theme.breakpoints.down("sm")]:{
+        width: '58vw',
+        margin: theme.spacing(2.7, 3.3, 0, 0),
+      },
+      width: '30vw',
+      // Icons style:
+      "& h4": {
+        margin: theme.spacing(8, 0, 0, 4),
+        fontSize: '1.4rem',
+        fontWeight: 'bold',
+        color: '#A0522D',
+        textShadow: "1px .6px #FCFF53",
+      },
+      "& h4:hover, &.Mui-focusVisible": {
+        cursor: 'pointer',
+        fontWeight: 'bold',
+        transition: 'all ease 0.3s',
+        color: "tomato",
+        textShadow: "1px .7px  #A0522D",
+        borderBottom: "2.5px solid tomato",
+    },
     },
     root: {
         backgroundColor: "#FFE082",
