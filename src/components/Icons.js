eea7e85a25b29136bf2f8620cfd8daa280d562e8
brachintosh <bedendo.br@gmail.com>
2022-06-29 12:08:06 -0300

    added-icons-grid-comp

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,6 +2,7 @@ import { makeStyles, Paper, Typography, Grid  } from '@material-ui/core';
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineOppositeContent, TimelineSeparator } from '@material-ui/lab';
 // ICONS  //
 import canva from '../images/canva.png';
+import mySQL from '../images/mySQL.png';
 import figmaIcon from '../images/icons8-figma2.svg';
 import nodeJsIcon from '../images/nodeJS-icon2.png';
 import expressIcon from '../images/expresIcon.png';
@@ -88,6 +89,12 @@ const Icons = () => {
           stars: 4,
         },
         {
+          area: 'Data-Base',
+          src: mySQL,
+          title: "mySQL",
+          stars: 4,
+        },
+        {
           src: postmanIcon,
           title: "Postman",
           stars: 4,
@@ -127,7 +134,7 @@ const Icons = () => {
 
         <Grid item xs={6}>
           <Paper className={classes.paper}>
-                <img src={canva} alt={canva} title={canva} className={classes.customLogo} />
+                <img src={canva} alt={canva} title="Canva" className={classes.customLogo} />
           </Paper>
         </Grid>
 
@@ -161,6 +168,39 @@ const Icons = () => {
           </Paper>
         </Grid>
 
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <img src={techSkills[3].src} alt={techSkills[3].title} title={techSkills[3].title} className={classes.customLogo} />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <img src={techSkills[13].src} alt={techSkills[13].title} title={techSkills[13].title} className={classes.customLogo} />
+          </Paper>
+        </Grid>
+
+        {/* DATA BASE */}
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant='subtitle' color='inherit' >
+                {techSkills[11].area}
+            </Typography>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <img src={techSkills[11].src} alt={techSkills[11].title} title={techSkills[11].title} className={classes.customLogo} />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <img src={techSkills[12].src} alt={techSkills[12].title} title={techSkills[12].title} className={classes.customLogo} />
+          </Paper>
+        </Grid>
+
         {/* FRONT END */}
         <Grid item xs={12}>
           <Paper className={classes.paper}>
@@ -169,35 +209,49 @@ const Icons = () => {
             </Typography>
           </Paper>
         </Grid>
-        
-        <Grid item xs={3}>
+       
+        <Grid item xs={4}>
           <Paper className={classes.paper}>
-            <Typography variant='subtitle' color='inherit' >
-                Design 3
-            </Typography>
+            <img src={techSkills[4].src} alt={techSkills[4].title} title={techSkills[4].title} className={classes.customLogo} />
           </Paper>
         </Grid>
-        <Grid item xs={3}>
+
+        <Grid item xs={4}>
           <Paper className={classes.paper}>
-          <Typography variant='subtitle' color='inherit' >
-                Design 3
-            </Typography>
+            <img src={techSkills[5].src} alt={techSkills[5].title} title={techSkills[5].title} className={classes.customLogo} />
           </Paper>
         </Grid>
-        <Grid item xs={3}>
+       
+        <Grid item xs={4}>
           <Paper className={classes.paper}>
-          <Typography variant='subtitle' color='inherit' >
-                Design 3
-            </Typography>
+            <img src={techSkills[6].src} alt={techSkills[6].title} title={techSkills[6].title} className={classes.customLogo} />
           </Paper>
         </Grid>
-        <Grid item xs={3}>
+
+        <Grid item xs={4}>
           <Paper className={classes.paper}>
-          <Typography variant='subtitle' color='inherit' >
-                Design 3
-            </Typography>
+            <img src={techSkills[7].src} alt={techSkills[7].title} title={techSkills[7].title} className={classes.customLogo} />
           </Paper>
         </Grid>
+
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <img src={techSkills[8].src} alt={techSkills[8].title} title={techSkills[8].title} className={classes.customLogo} />
+          </Paper>
+        </Grid>
+       
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <img src={techSkills[9].src} alt={techSkills[9].title} title={techSkills[9].title} className={classes.customLogo} />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <img src={techSkills[10].src} alt={techSkills[10].title} title={techSkills[10].title} className={classes.customLogo} />
+          </Paper>
+        </Grid>
+
       </Grid>
     </div>
   )
